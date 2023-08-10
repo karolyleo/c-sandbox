5c4eb1725c58edd565036b88b7bf4f38861aa3bc
karolyleo <leonard.a.karoly@gmail.com>
2023-08-10 17:59:21 -0500

    C# guessing game

---
+++ hello-world/Program.cs
@@ -0,0 +1,53 @@
+﻿namespace HelloWorld
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Random random= new Random();
+            Console.Out.WriteLine("Let's play a guessing game");
+            int ranDum = random.Next(1, 10);
+            bool playOn = true;
+
+            while (playOn)
+            {
+                Console.WriteLine("Guess a number");
+                int userInput;
+
+                try {
+                    userInput = Convert.ToInt32(Console.ReadLine());
+
+                    highLow(userInput, ranDum);
+
+                    playOn = keepPlaying(userInput);
+                    
+                } catch (Exception ex)
+                {
+                    Console.Out.WriteLine(ex.Message);
+                }
+            }
+        }
+        static void highLow(int guess, int random)
+        {
+            string response;
+            if (guess < random)
+            {
+                response = "Too low";
+            } else if (guess > random)
+            {
+                response = "Too high";
+            } else 
+            {
+                response = "Nice guess, you win!";
+            }
+            Console.Out.WriteLine(response);
+        }
+
+        static bool keepPlaying(int userInput)
+        {
+            Console.WriteLine("If you want to stop playing, input 0, you have selected: " + userInput);
+
+            return userInput != 0;            
+        }
+    }
+}
