0836dc84358a303acf3ed07eb578db41abae5c94
karolyleo <leonard.a.karoly@gmail.com>
2023-08-16 10:52:58 -0500

    Random guessing game

--- hello-world/Program.cs
+++ hello-world/Program.cs
@@ -4,33 +4,87 @@
     {
         public static void Main(string[] args)
         {
+            string filePath = txtToLog();         
+
+            string[] gameResult = guesingGame();
+
+            File.AppendAllLines(filePath, gameResult);
+        }
+
+        static string txtToLog()
+        {
+            // Hardcoded path
+            string folderPath = "/home/karolleo000/IdeaProjects/c-sandbox/c-sandbox/hello-world/Game-logs/";
+
+            // File to be located
+            string filePath = Path.Combine(folderPath, "Previous-Game.txt");
+
+            // Creation/Verification of file
+            try
+            {
+                Directory.CreateDirectory(folderPath);
+
+                // Ensure our stuff runs!
+                if (!File.Exists(filePath))
+                {
+                    File.WriteAllText(filePath, "This is a new file.");
+                    Console.WriteLine("First time playing, welcome!");
+                } 
+                else 
+                {
+                    Console.WriteLine("Resetting Saved Game");
+                }
+            }
+            catch (Exception ex)
+            {
+                // Note the $ before our " " and variables can be place inside of { }
+                Console.WriteLine($"An error occured: {ex.Message}");
+            }
+
+            // return path
+            return filePath;
+        }
+        static string[] guesingGame()
+        {
             Random random = new Random();
-            // Console.Out.WriteLine("Let's play a guessing game");
-            int ranDum = random.Next(1, 10), num = 0;
+
+            Console.Out.WriteLine("Let's play a guessing game");
+            int ranDum = random.Next(1, 10), tries = 0;
             bool playOn = true;
+            List<string> gameLog = new List<string>();
+            gameLog.Add("----------New Game----------");
+            gameLog.Add($"The random number generated is {ranDum}");
 
             while (playOn)
             {
                 Console.WriteLine("Guess a number");
                 int userInput;
-                for(int i = 0; i < num;i++)
-                {
-                Console.WriteLine("Hey you!");
-                }
-                num++;
-
-                if (num > 99)
-                {
-                    playOn = false;
-                }
+            
+                tries++;
 
                 try
                 {
                     userInput = Convert.ToInt32(Console.ReadLine());
 
-                    highLow(userInput, ranDum);
+                    // Custom msg for the file
+                    gameLog.Add($"On try {tries}, the user guessed {userInput}");
 
-                    playOn = keepPlaying(userInput);
+                    if ( !highLow(userInput, ranDum)) 
+                    {
+                        gameLog.Add("Congrats on Winning!");
+                        playOn = false;
+                        break;
+                    }
+
+                    if (tries > 3)
+                {
+                    playOn = false;
+                    string loss = "All tries used up... You lost!";
+                    Console.WriteLine(loss);
+
+                    // Add to our log
+                    gameLog.Add(loss);
+                }
 
                 }
                 catch (Exception ex)
@@ -38,8 +92,10 @@
                     Console.Out.WriteLine(ex.Message);
                 }
             }
+            return gameLog.ToArray();
         }
-        static void highLow(int guess, int random)
+
+        static bool highLow(int guess, int random)
         {
             int comparisonResult = guess.CompareTo(random);
 
@@ -47,21 +103,15 @@
             {
                 case -1:
                     Console.WriteLine("Too low");
-                    break;
+                    return true;
                 case 1:
                     Console.WriteLine("Too high");
-                    break;
+                    return true;
                 default:
                     Console.WriteLine("Nice guess, you win!");
-                    break;
+                    return false;
             }
         }
 
-        static bool keepPlaying(int userInput)
-        {
-            Console.WriteLine("If you want to stop playing, input 0, you have selected: " + userInput);
-
-            return userInput != 0;
-        }
     }
 }
